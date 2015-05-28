68fd0df92073e0b802225e18f3228bbd95cc6055
David Thevenin <david.thevenin@gmail.com>
2015-05-28 18:24:06 +0900

    Update data

---
+++ capoeiraMoves.js
@@ -0,0 +1,45 @@
+var app = angular.module ('CapoeiraMoves', ['ngMaterial']);
+
+app.config (function ($mdIconProvider) {
+  $mdIconProvider.icon ("menu", "/assets/menu.svg", 24);
+});
+
+app.controller('AppCtrl', function($scope, $mdSidenav, $sce){
+
+  function patchData () {
+    technics.forEach (function (sub_item) {
+      sub_item.list.forEach (function (item) {
+        item.type = sub_item.type;
+        item.sub_type = sub_item.sub_type;
+      });
+    });
+  }
+  patchData ();
+
+  $scope.technics = technics;//[0].list;
+
+  $scope.technics.selected = technics[0].list [0];
+
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+ 
+  $scope.technics.selectMove = function (item) {
+    $scope.technics.selected = item;
+  }
+
+  $scope.generateYoutubeLink = function (code) {
+    return $sce.trustAsResourceUrl ("https://www.youtube.com/embed/" + code);
+  }
+  
+  $scope.generateLocalLink = function (code) {
+    return $sce.trustAsResourceUrl ("" + code);
+  }
+  
+  $scope.selectedLevels = [0];//[0, 1]; 
+
+  $scope.contains = function (expected, actual) {  
+    return actual.indexOf(expected) > -1;
+  };
+
+});
